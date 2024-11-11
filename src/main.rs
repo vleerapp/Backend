b0ad21d2d3f26c1e40a841661fc1a97a7939c3b9
PandaDEV <70103896+0PandaDEV@users.noreply.github.com>
2024-11-11 21:07:19 +1000

    fix: only allow localhost origin

--- src/main.rs
+++ src/main.rs
@@ -42,7 +42,7 @@ async fn main() -> std::io::Result<()> {
 
     let server = HttpServer::new(move || {
         let cors = Cors::default()
-            .allowed_origin("http://localhost:3000")
+            .allow_any_origin()
             .allowed_methods(vec!["GET", "POST"])
             .allowed_headers(vec![header::AUTHORIZATION, header::ACCEPT])
             .allowed_header(header::CONTENT_TYPE)
