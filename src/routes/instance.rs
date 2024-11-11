99c6496cb89a52b1a13732403e40f2d33a0bac91
PandaDEV <70103896+0PandaDEV@users.noreply.github.com>
2024-11-11 17:26:26 +1000

    fix: instances response format

--- src/routes/instance.rs
+++ src/routes/instance.rs
@@ -4,11 +4,6 @@ use serde::Serialize;
 use crate::piped::get_instances;
 
 #[derive(Serialize)]
-struct InstanceLatencyResponse {
-    instances: Vec<InstanceLatency>,
-}
-
-#[derive(Serialize)]
 struct InstanceLatency {
     name: String,
     api_url: String,
@@ -18,13 +13,13 @@ struct InstanceLatency {
 pub async fn instances_route() -> impl Responder {
     let instances = get_instances();
 
-    HttpResponse::Ok().json(InstanceLatencyResponse {
-        instances: instances
+    HttpResponse::Ok().json(
+        instances
             .into_iter()
             .map(|instance| InstanceLatency {
                 name: instance.name,
-                api_url: instance.api_url,
+                api_url: instance.api_url
             })
-            .collect(),
-    })
+            .collect::<Vec<InstanceLatency>>()
+    )
 }
